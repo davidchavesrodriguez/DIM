80659d5e4e7a77970db752123e9abf55c712b7ad
A22DavidCR <A22DavidCR@iessanclemente.net>
2024-09-18 10:36:30 +0200

    EstructuraWebBasica done

--- UD1/estructuraWebBasica/EstructuraWebBasica/src/main/java/org/example/EstructuraWebBasica.java
+++ UD1/estructuraWebBasica/EstructuraWebBasica/src/main/java/org/example/EstructuraWebBasica.java
@@ -9,17 +9,23 @@ public class EstructuraWebBasica {
         jframe.setTitle("Estructura Web Basica");
         jframe.setSize(500, 500);
         jframe.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-
+//        jframe.setBackground(Color.white);
         jframe.setLayout(new BorderLayout());
 
         JPanel mainHeader = new JPanel();
         mainHeader.setBackground(Color.decode("#cd5c5c"));
-        mainHeader.add(new JLabel("<header>"));
+        JLabel headerLabel = new JLabel("<header>");
+        headerLabel.setForeground(Color.WHITE);
+        mainHeader.add(headerLabel);
+        mainHeader.setBorder(BorderFactory.createMatteBorder(0, 0, 10, 0, Color.white));
         jframe.add(mainHeader, BorderLayout.NORTH);
 
         JPanel mainFooter = new JPanel();
         mainFooter.setBackground(Color.decode("#cd5c5c"));
-        mainFooter.add(new JLabel("<footer>"));
+        JLabel footerLabel = new JLabel("<footer>");
+        footerLabel.setForeground(Color.WHITE);
+        mainFooter.add(footerLabel);
+        mainFooter.setBorder(BorderFactory.createMatteBorder(10, 0, 0, 0, Color.white));
         jframe.add(mainFooter, BorderLayout.SOUTH);
 
         JPanel mainPanel = new JPanel();
@@ -28,33 +34,56 @@ public class EstructuraWebBasica {
 
         JPanel navPanel = new JPanel();
         navPanel.setBackground(Color.decode("#cd5c5c"));
-        navPanel.add(new JLabel("<nav>"));
+        JLabel navLabel = new JLabel("<nav>", JLabel.CENTER);
+        navLabel.setForeground(Color.WHITE);
+        navPanel.setLayout(new BorderLayout());
+        navPanel.add(navLabel, BorderLayout.CENTER);
         mainPanel.add(navPanel);
 
         JPanel sectionPanel = new JPanel();
         sectionPanel.setLayout(new GridLayout(4, 1));
         sectionPanel.setBackground(Color.decode("#cd5c5c"));
+        JLabel sectionLabel = new JLabel("<section>", JLabel.CENTER);
+        sectionLabel.setForeground(Color.WHITE);
+        sectionPanel.add(sectionLabel);
+        sectionPanel.setBorder(BorderFactory.createMatteBorder(0, 10, 0, 10, Color.white));
 
         JPanel sectionHeader = new JPanel();
+        sectionHeader.setSize(1, 1);
         sectionHeader.setBackground(Color.decode("#bdb76b"));
-        sectionHeader.add(new JLabel("<header>"));
+        JLabel sectionHeaderLabel = new JLabel("<header>", JLabel.CENTER);
+        sectionHeaderLabel.setForeground(Color.WHITE);
+        sectionHeader.setLayout(new BorderLayout());
+        sectionHeader.setBorder(BorderFactory.createMatteBorder(5, 5, 5, 5, Color.decode("#cd5c5c")));
+        sectionHeader.add(sectionHeaderLabel, BorderLayout.CENTER);
         sectionPanel.add(sectionHeader);
 
         JPanel sectionArticle = new JPanel();
         sectionArticle.setBackground(Color.decode("#bdb76b"));
-        sectionArticle.add(new JLabel("<article>"));
+        JLabel sectionArticleLabel = new JLabel("<article>", JLabel.CENTER);
+        sectionArticleLabel.setForeground(Color.WHITE);
+        sectionArticle.setLayout(new BorderLayout());
+        sectionArticle.setBorder(BorderFactory.createMatteBorder(5, 5, 5, 5, Color.decode("#cd5c5c")));
+        sectionArticle.add(sectionArticleLabel, BorderLayout.CENTER);
         sectionPanel.add(sectionArticle);
 
         JPanel sectionFooter = new JPanel();
         sectionFooter.setBackground(Color.decode("#bdb76b"));
-        sectionFooter.add(new JLabel("<footer>"));
+        JLabel sectionFooterLabel = new JLabel("<footer>", JLabel.CENTER);
+        sectionFooterLabel.setForeground(Color.WHITE);
+        sectionFooter.setLayout(new BorderLayout());
+        sectionFooter.setBorder(BorderFactory.createMatteBorder(5, 5, 5, 5, Color.decode("#cd5c5c")));
+        sectionFooter.add(sectionFooterLabel, BorderLayout.CENTER);
         sectionPanel.add(sectionFooter);
 
         mainPanel.add(sectionPanel);
 
         JPanel asidePanel = new JPanel();
         asidePanel.setBackground(Color.decode("#cd5c5c"));
-        asidePanel.add(new JLabel("<aside>"));
+        JLabel asideLabel = new JLabel("<aside>", JLabel.CENTER);
+        asideLabel.setForeground(Color.WHITE);
+        asidePanel.setLayout(new BorderLayout());
+        asidePanel.add(asideLabel, BorderLayout.CENTER);
         mainPanel.add(asidePanel);
 
         jframe.setVisible(true);
